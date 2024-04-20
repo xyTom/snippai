30bb863546428ed7e0ca086a17dc4a086c96287f
xyTom <75466119+xyTom@users.noreply.github.com>
2024-04-20 19:54:47 +0000

    修复模型选择中的图片URL问题

--- src/renderer/models/model-gpt4.ts
+++ src/renderer/models/model-gpt4.ts
@@ -1,24 +1,61 @@
 import OpenAI from 'openai';
 async function GPT(image:string, prompt:string, APIKey:string): Promise<string> {
-    const openai = new OpenAI({ apiKey: APIKey, dangerouslyAllowBrowser: true});
-    const response = await openai.chat.completions.create({
-        model: "gpt-4-vision-preview",
-        messages: [
-          {
-            role: "user",
-            content: [
-              { type: "text", text: prompt },
-              {
-                type: "image_url",
-                image_url: {
-                    "url": `data:image/jpeg;base64,${image}`,
-                },
-              },
-            ],
-          },
-        ],
-      });
-      console.log(response.choices[0]);
-      return response.choices[0].toString();
+    const myHeaders = new Headers();
+    myHeaders.append("authorization", `Bearer ${APIKey}`);
+    myHeaders.append("content-type", "application/json");
+    const raw = JSON.stringify({
+      "model": "gpt-4-vision-preview",
+      "messages": [
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": "text",
+              "text": prompt
+            },
+            {
+              "type": "image_url",
+              "image_url": {
+                "url": `data:image/jpeg;base64,${image}`,
+              }
+            }
+          ]
+        }
+      ]
+    });
+    
+    const requestOptions: RequestInit = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow" as RequestRedirect
+    };
+    
+    const response = await fetch("https://api.openai.com/v1/chat/completions", requestOptions);
+    const result = await response.json();
+    return result["choices"][0]["message"]["content"];
+  
+    
+    
+    // const openai = new OpenAI({ apiKey: APIKey, dangerouslyAllowBrowser: true});
+    // const response = await openai.chat.completions.create({
+    //     model: "gpt-4-vision-preview",
+    //     messages: [
+    //       {
+    //         role: "user",
+    //         content: [
+    //           { type: "text", text: prompt },
+    //           {
+    //             type: "image_url",
+    //             image_url: {
+    //                 "url": `data:image/jpeg;base64,${image}`,
+    //             },
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   });
+    //   console.log(response.choices[0]);
+    //   return response.choices[0].toString();
 }
 export default GPT;
