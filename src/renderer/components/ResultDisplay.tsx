7309de035d7c3df64609b72a3755d214846cf9fa
xytom <75466119+xyTom@users.noreply.github.com>
2025-04-08 11:19:55 -0700

    Fix file import case sensitivity issue in ResultDisplay.tsx

--- src/renderer/components/ResultDisplay.tsx
+++ src/renderer/components/ResultDisplay.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { MathJaxContext } from 'better-react-mathjax';
 import LoadingSkeleton from './loadingSkeleton';
 import DisplayLatex from './displayLatex';
-import DisplayTextResult from './DisplayTextResult';
+import DisplayTextResult from './displayTextResult';
 
 interface ResultDisplayProps {
   loading: boolean;
